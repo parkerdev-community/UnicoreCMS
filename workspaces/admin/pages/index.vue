05155ec0b01c4916cd6bb8011da9442c4d600a3f
ElChapo193 <ivantrufanovq@yandex.ru>
2022-04-01 19:02:40 +0300

    upd

--- workspaces/admin/pages/index.vue
+++ workspaces/admin/pages/index.vue
@@ -124,149 +124,18 @@
         <Chart type="bar" :data="barData" :options="barOptions" />
       </div>
     </div>
-    <div class="col-12 xl:col-6 mt-2">
+    <div class="col-12 mt-2">
       <div class="card h-full">
-        <div class="flex justify-content-between align-items-center mb-5">
-          <h5>Best Selling Products</h5>
-          <div>
-            <Button
-              icon="pi pi-ellipsis-v"
-              class="p-button-text p-button-plain p-button-rounded"
-              @click="$refs.menu2.toggle($event)"
-            ></Button>
-            <!--Menu ref="menu2" :popup="true" :model="items"></Menu-->
-          </div>
+        <div class="flex justify-content-between align-items-center mb-4">
+          <h5>Текущий статус серверов</h5>
         </div>
         <ul class="list-none p-0 m-0">
-          <li class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
+          <li v-for="online in onlines.servers" :key="online.server.id" class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
             <div>
-              <span class="text-900 font-medium mr-2 mb-1 md:mb-0">Space T-Shirt</span>
-              <div class="mt-1 text-600">Clothing</div>
+              <span class="text-900 font-medium mr-2 mb-1 md:mb-0" v-text="online.server.name" />
+              <div v-if="online.online" class="mt-1 text-600">{{ online.players }}/{{ online.maxplayers }}, рекорд: {{ online.record }}, cегодня {{ online.record_today }}</div>
+              <div v-else class="mt-1 text-600">Оффлайн, рекорд: {{ online.record }}, cегодня {{ online.record_today }}</div>
             </div>
-            <div class="mt-2 md:mt-0 flex align-items-center">
-              <div class="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style="height: 8px">
-                <div class="bg-orange-500 h-full" style="width: 50%"></div>
-              </div>
-              <span class="text-orange-500 ml-3 font-medium">%50</span>
-            </div>
-          </li>
-          <li class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-            <div>
-              <span class="text-900 font-medium mr-2 mb-1 md:mb-0">Portal Sticker</span>
-              <div class="mt-1 text-600">Accessories</div>
-            </div>
-            <div class="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-              <div class="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style="height: 8px">
-                <div class="bg-cyan-500 h-full" style="width: 16%"></div>
-              </div>
-              <span class="text-cyan-500 ml-3 font-medium">%16</span>
-            </div>
-          </li>
-          <li class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-            <div>
-              <span class="text-900 font-medium mr-2 mb-1 md:mb-0">Supernova Sticker</span>
-              <div class="mt-1 text-600">Accessories</div>
-            </div>
-            <div class="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-              <div class="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style="height: 8px">
-                <div class="bg-pink-500 h-full" style="width: 67%"></div>
-              </div>
-              <span class="text-pink-500 ml-3 font-medium">%67</span>
-            </div>
-          </li>
-          <li class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-            <div>
-              <span class="text-900 font-medium mr-2 mb-1 md:mb-0">Wonders Notebook</span>
-              <div class="mt-1 text-600">Office</div>
-            </div>
-            <div class="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-              <div class="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style="height: 8px">
-                <div class="bg-green-500 h-full" style="width: 35%"></div>
-              </div>
-              <span class="text-green-500 ml-3 font-medium">%35</span>
-            </div>
-          </li>
-          <li class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-            <div>
-              <span class="text-900 font-medium mr-2 mb-1 md:mb-0">Mat Black Case</span>
-              <div class="mt-1 text-600">Accessories</div>
-            </div>
-            <div class="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-              <div class="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style="height: 8px">
-                <div class="bg-purple-500 h-full" style="width: 75%"></div>
-              </div>
-              <span class="text-purple-500 ml-3 font-medium">%75</span>
-            </div>
-          </li>
-          <li class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
-            <div>
-              <span class="text-900 font-medium mr-2 mb-1 md:mb-0">Robots T-Shirt</span>
-              <div class="mt-1 text-600">Clothing</div>
-            </div>
-            <div class="mt-2 md:mt-0 ml-0 md:ml-8 flex align-items-center">
-              <div class="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style="height: 8px">
-                <div class="bg-teal-500 h-full" style="width: 40%"></div>
-              </div>
-              <span class="text-teal-500 ml-3 font-medium">%40</span>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="col-12 xl:col-6 mt-2">
-      <div class="card h-full">
-        <div class="flex align-items-center justify-content-between mb-4">
-          <h5>Notifications</h5>
-          <div>
-            <Button
-              icon="pi pi-ellipsis-v"
-              class="p-button-text p-button-plain p-button-rounded"
-              @click="$refs.menu1.toggle($event)"
-            ></Button>
-            <Menu ref="menu1" :popup="true" :model="items"></Menu>
-          </div>
-        </div>
-
-        <span class="block text-600 font-medium mb-3">TODAY</span>
-        <ul class="p-0 mx-0 mt-0 mb-4 list-none">
-          <li class="flex align-items-center py-2 border-bottom-1 surface-border">
-            <div class="w-3rem h-3rem flex align-items-center justify-content-center bg-blue-100 border-circle mr-3 flex-shrink-0">
-              <i class="pi pi-dollar text-xl text-blue-500"></i>
-            </div>
-            <span class="text-900 line-height-3"
-              >Richard Jones
-              <span class="text-700">has purchased a blue t-shirt for <span class="text-blue-500">79$</span></span>
-            </span>
-          </li>
-          <li class="flex align-items-center py-2">
-            <div class="w-3rem h-3rem flex align-items-center justify-content-center bg-orange-100 border-circle mr-3 flex-shrink-0">
-              <i class="pi pi-download text-xl text-orange-500"></i>
-            </div>
-            <span class="text-700 line-height-3"
-              >Your request for withdrawal of <span class="text-blue-500 font-medium">2500$</span> has been initiated.</span
-            >
-          </li>
-        </ul>
-
-        <span class="block text-600 font-medium mb-3">YESTERDAY</span>
-        <ul class="p-0 m-0 list-none">
-          <li class="flex align-items-center py-2 border-bottom-1 surface-border">
-            <div class="w-3rem h-3rem flex align-items-center justify-content-center bg-blue-100 border-circle mr-3 flex-shrink-0">
-              <i class="pi pi-dollar text-xl text-blue-500"></i>
-            </div>
-            <span class="text-900 line-height-3"
-              >Keyser Wick
-              <span class="text-700">has purchased a black jacket for <span class="text-blue-500">59$</span></span>
-            </span>
-          </li>
-          <li class="flex align-items-center py-2 border-bottom-1 surface-border">
-            <div class="w-3rem h-3rem flex align-items-center justify-content-center bg-pink-100 border-circle mr-3 flex-shrink-0">
-              <i class="pi pi-question text-xl text-pink-500"></i>
-            </div>
-            <span class="text-900 line-height-3"
-              >Jane Davis
-              <span class="text-700">has posted a new questions about your product.</span>
-            </span>
           </li>
         </ul>
       </div>
@@ -275,6 +144,8 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   data() {
     return {
@@ -311,11 +182,21 @@ export default {
       },
     }
   },
-  themeChangeListener: null,
   async fetch() {
     this.stats = await this.$axios.get('/admin/dashboard/stats').then((res) => res.data)
 
     this.daysCharts()
+  },
+  computed: {
+    ...mapGetters({
+      onlines: 'io/serversOnline'
+    }),
+  },
+  async mounted() {
+    this.socket = this.$nuxtSocket({})
+    this.socket.emit('servers/online', {}, (res) => {
+      this.$store.commit('io/SERVERS_ONLINE', res)
+    })
   },
   methods: {
     formatCurrency(value) {
