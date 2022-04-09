70e0973b09e6cdfefc3a585e0f78ef7cad1d1573
ElChapo193 <ivantrufanovq@yandex.ru>
2022-04-09 15:14:48 +0300

    Fixes

--- workspaces/admin/middleware/admin.ts
+++ workspaces/admin/middleware/admin.ts
@@ -1,7 +1,9 @@
 import { Middleware } from '@nuxt/types'
 
-const adminMiddleware: Middleware = ({ $auth, redirect, $config }) => {
-  if ($auth.user?.perms && !($auth.user?.perms as string[]).find(perm => perm == "admin.dashboard")) return redirect($config.url)
+const adminMiddleware: Middleware = ({ $auth }) => {
+  if (!$auth.user) return $auth.redirect("login")
+  if ($auth.user?.perms && !($auth.user?.perms as string[]).find(perm => perm == "admin.dashboard"))
+    return $auth.redirect("login")
 }
 
 export default adminMiddleware
