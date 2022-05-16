e033076543b72f5ebfccdecd108ab8053e1bde0a
ElChapo193 <ivantrufanovq@yandex.ru>
2022-05-16 15:02:57 +0300

    [FIX] ql-align & ormconfig bundle

--- workspaces/server/webpack.config.js
+++ workspaces/server/webpack.config.js
@@ -5,11 +5,7 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const WebpackObfuscator = require('webpack-obfuscator');
 
 const main = module.exports = {
-  entry: {
-    main: './src/main.ts',
-    ormconfig: './src/ormconfig.ts',
-    "ormconfig-schema": './src/ormconfig-schema.ts'
-  },
+  entry: "./src/main.ts",
   target: 'node',
   mode: "production",
   output: {
@@ -22,6 +18,37 @@ const main = module.exports = {
       target: "node"
     })
   ],
+  externals: [
+    nodeExternals(),
+    nodeExternals({
+      modulesDir: path.resolve(__dirname, '../../node_modules'),
+    })
+  ],
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  optimization: {
+    minimize: false,
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.build.json' })],
+  },
+  module: {
+    rules: [{ test: /\.ts$/, loader: 'ts-loader' }],
+  }
+};
+
+const orm = module.exports = {
+  entry: './src/ormconfig-schema.ts',
+  target: 'node',
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2',
+    filename: 'ormconfig-schema.js',
+  },
   externals: [
     nodeExternals(),
     nodeExternals({
@@ -61,4 +88,4 @@ const cli = {
 }
 
 
-module.exports = [main, cli]
+module.exports = [orm, main, cli]
