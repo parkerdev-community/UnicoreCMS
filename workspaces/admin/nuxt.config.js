fb51654d7b62e09028be112de04b470c43356d45
ElChapo193 <ivantrufanovq@yandex.ru>
2022-05-16 06:00:40 +0300

    [UPD] UnicoreAdmin obfuscated

--- workspaces/admin/nuxt.config.js
+++ workspaces/admin/nuxt.config.js
@@ -1,5 +1,6 @@
 import mainConfig from './main.config'
 import { envConfig } from 'unicore-common/dist/envconfig'
+const WebpackObfuscator = require('webpack-obfuscator');
 
 export default mainConfig({
   ssr: false,
@@ -121,4 +122,16 @@ export default mainConfig({
   router: {
     middleware: ['auth'],
   },
+
+  build: {
+    extend(config, { isDev, isClient }) {
+      if (!isDev && isClient && config.plugins) {
+        config.plugins.push(
+          new WebpackObfuscator({
+            rotateStringArray: true
+          }, ['node_modules/**/*.js', '../../node_modules/**/*.js'])
+        );
+      }
+    }
+  }
 })
