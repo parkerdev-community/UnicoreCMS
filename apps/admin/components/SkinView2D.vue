59c592cd47225bc8cd802551a05edc381180bd38
ParkerDeveloper <event.roleplay.1@mail.ru>
2024-06-14 23:45:16 +0300

    Начало перехода на турбо репу

---
+++ apps/admin/components/SkinView2D.vue
@@ -0,0 +1,53 @@
+<template>
+  <div ref="skinHead" :style="`max-width: ${width}px; max-height: ${height}px`" />
+</template>
+<script>
+import { SkinViewer2D } from '../libs/skinview2d'
+
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 64,
+    },
+    height: {
+      type: Number,
+      default: 64,
+    },
+    skin: Object,
+  },
+
+  watch: {
+    skin: {
+      handler: function () {
+        this.render()
+      },
+      deep: true,
+    },
+    width() {
+      this.render()
+    },
+    height() {
+      this.render()
+    },
+  },
+
+  mounted() {
+    this.render()
+  },
+
+  methods: {
+    render() {
+      new SkinViewer2D({
+        domElement: this.$refs.skinHead,
+        skinUrl:
+          this.skin && this.$_.get(this.skin, 'file')
+            ? `${this.$config.apiUrl}/${this.$_.get(this.skin, 'file')}`
+            : `${this.$config.url}/default.png`,
+        width: this.width,
+        height: this.height,
+      })
+    },
+  },
+}
+</script>
